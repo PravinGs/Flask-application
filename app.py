5a43a9cbeb3eadfa7b3b0de9e5c561547e0512d4
pravinkumar150 <72332097+pravinkumar150@users.noreply.github.com>
2021-08-25 20:53:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask , render_template, request
+import jsonify
+import requests
+import pickle 
+import numpy as np
+import sklearn 
+from sklearn.preprocessing import StandardScaler
+
+app = Flask(__name__) 
+model = pickle.load(open('random_forest_model.pkl','rb'))
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+
+standard_to = StandardScaler()
+@app.route("/predict", methods=['POST'])
+def predict():
+    # Fuel_Type_Diesel = 0 
+    if request.method == 'POST':
+        Year = int(request.form['Year'])
+        PresentPrice = float(request.form['PresentPrice'])
+        KmsDriven = int(request.form['KmsDriven'])
+        KmsDriven2 = np.log(KmsDriven)
+        Owner = int(request.form['Owner'])
+        FuelType=request.form['FuelType']
+        if FuelType == 'Petrol':
+            FuelType = 0
+        elif FuelType == 'Diesel':
+            FuelType = 1
+        else:
+            FuelType = 2
+        Year = Year - 2021
+        SellerType = request.form['SellerType']
+        if SellerType == 'Individual':
+            SellerType = 1
+        else:
+            SellerType = 0
+        Tranmission = request.form['Tranmission']
+        if Tranmission == 'Mannual':
+            Tranmission = 0
+        else:
+            Tranmission = 1
+        prediction = model.predict([[PresentPrice,KmsDriven,FuelType,SellerType,Tranmission,Owner,Year]])
+        output = round(prediction[0],2)
+        if output < 0:
+            return render_template('index.html',prediction_texts="Sorry you cannot sell this car.")
+        else:
+            return render_template('sucess.html',prediction_text=f"You can sell The car at <strong>{output}</strong>")
+    else:
+        return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+
+
+
+        
+
